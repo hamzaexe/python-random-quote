3c301feb2b29307c803cfe7a630faee9f6dd65c6
hamza nadeem <hnb351@gmail.com>
2020-12-30 03:11:24 +0000

    adding more line function and print 2 lines

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,24 @@
 import random
 def primary():
-  last = 13
-  rnd = random.randint(0, last)
-  #print("Keep it logically awesome.")
+
+  a = open("quotes.txt", "a")
+  input_sentence=input("write your sentence: ")
+  a.write(input_sentence + "\n")
+  a.close()
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
-  print(quotes[rnd])
+  last = len(quotes)-1
+  print(last)
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  #print("Keep it logically awesome.")
+
+ 
+
+  print(quotes[rnd], end = '')
+  rnd = random.randint(0, last)
+  print(quotes[rnd], end = '')
 
 if __name__== "__main__":
   primary()
